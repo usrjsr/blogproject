38df6ca6dc80e267ff1bc3916688be65ddf40ec5
usrjsr <utkarshsinghdau@gmail.com>
2025-07-14 10:47:11 +0530

    Fix: imported login action for Signup redirect

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import authService from '../appwrite/auth'
 import {Link ,useNavigate} from 'react-router-dom'
+import { login } from '../store/auth.slice';
 
 
 import {Button, Input, Logo} from './index.js'
